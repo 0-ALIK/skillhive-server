9460ddb53a63ec45e07fe992ef1e3309ad2c78e7
Alik <moisesalik01@gmail.com>
2024-05-20 21:57:49 -0500

    migraciones

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,13 +11,8 @@ export const dataSource = new DataSource({
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'skillhive-dev',
     logging: process.env.DB_LOGS === 'true' ? true : false,
-    entities: ['src/entities/**/*.ts'],
+    synchronize: true,
+    entities: ['src/entity/**/*.ts'],
     migrations: ['src/migrations/**/*.ts'],
     migrationsTableName: "_migration_table",
-    pool: {
-        max: 10,
-        min: 1, 
-        idleTimeoutMillis: 30000, 
-        acquireTimeoutMillis: 30000
-    }
 });
--- src/services/database.connection.ts
+++ src/services/database.connection.ts
@@ -1,5 +1,4 @@
 import { DataSource } from 'typeorm';
-import { dataSource } from '../data-source';
 
 export class DatabaseConnectionService {
     private static dataSource: DataSource;
@@ -7,7 +6,24 @@ export class DatabaseConnectionService {
     public static async databaseConnection(): Promise<DataSource> {
         try {
             if (!DatabaseConnectionService.dataSource) {
-                DatabaseConnectionService.dataSource = dataSource;
+                DatabaseConnectionService.dataSource = new DataSource({
+                    type: process.env.DB_TYPE as any || 'mysql',
+                    host: process.env.DB_HOST || 'localhost',
+                    port: Number(process.env.DB_PORT) || 3306,
+                    username: process.env.DB_USER || 'root',
+                    password: process.env.DB_PASS || '',
+                    database: process.env.DB_NAME || 'skillhive-dev',
+                    logging: process.env.DB_LOGS === 'true' ? true : false,
+                    entities: ['src/entities/**/*.ts'],
+                    migrations: ['src/migrations/**/*.ts'],
+                    migrationsTableName: "_migration_table",
+                    pool: {
+                        max: 10,
+                        min: 1, 
+                        idleTimeoutMillis: 30000, 
+                        acquireTimeoutMillis: 30000
+                    }
+                });
 
                 await DatabaseConnectionService.dataSource.initialize();
                 console.log('Database connected\n\n');
